76f3f763d4e16579afadc5e78ee9523ac1d117b8
Ahmad Ronagh <mr.a.ronagh@gmail.com>
2021-06-14 23:06:59 +0430

    add project structure

---
+++ Assets/IdleFishHunt/Scripts/Installer/GameSceneInstaller.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class GameSceneInstaller : MonoInstaller
+{
+    public override void InstallBindings()
+    {
+               
+    }
+}
